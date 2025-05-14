8ca46a1d9ba1455e494046fbf996a3dc37e3c23c
Iftikhar Ahmed <ifti3061@gmail.com>
2025-05-15 01:07:10 +0600

    fix issue

--- api/index.php
+++ api/index.php
@@ -1,20 +1,3 @@
 <?php
-// api/index.php
 
-// Load Laravel's autoloader
-require __DIR__ . '/../vendor/autoload.php';
-
-// Load Laravel kernel
-require_once __DIR__ . '/../bootstrap/app.php';
-
-// Create the kernel
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-// Handle the request
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+require __DIR__ . "/../public/index.php";
